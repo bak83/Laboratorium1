0daf855c09cc8f3946e3092019add69f8ecc5688
bak83 <bak.dariusz@gmail.com>
2020-06-15 20:06:17 +0200

    create class Greyhound and Guy

---
+++ Laboratorium1/Greyhound.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Windows.Forms;
+
+
+namespace Laboratorium1
+{
+	public class Greyhound
+	{
+		public int StartingPosition;
+		public int RaceTrackLength;
+		public PictureBox MyPictureBox;
+		public int Location = 0;
+		public Random MyRandom;
+
+		public bool Run()
+		{
+			return true;
+		}
+
+		public void TakeStartingPosition()
+		{
+
+		}
+
+	}
+}
---
+++ Laboratorium1/Guy.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace Laboratorium1
+{
+	public class Guy
+	{
+		public string name;
+		//pulic Bet MyBet;
+		public int Cash;
+		public RadioButton MyRadioButton;
+		public Label MyLabel;
+
+		public void UpdateLabels()
+		{
+
+		}
+
+		public void ClearBet()
+		{
+
+		}
+		public bool PlaceBet(int amount, int DogToWin)
+		{
+			return true;
+		}
+
+		public void Collect(int Winner)
+		{
+
+		}
+
+
+	}
+}
